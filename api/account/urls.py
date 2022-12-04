f1409a0e62bf9bff4d8d0b6e139505b498953012
FofuHupopo <fofuhupopo@gmail.com>
2022-12-04 18:17:21 +0500

    asd

--- api/account/urls.py
+++ api/account/urls.py
@@ -6,8 +6,7 @@ from . import views
 urlpatterns = [
     path('representative/', views.GetRepresentativeView.as_view(), name="representative__representative"),
 
-    path('representative/passport/russian/', views.PassportView.as_view(), name="representative__passport"),
-    path('representative/passport/foreign/', views.PassportView.as_view(), name="representative__passport"),
+    path('representative/passport/', views.PassportView.as_view(), name="representative__passport"),
 
     path('representative/address/', views.RepresentativeAddressView.as_view(), name="representative__address"),
     path('representative/child/', views.ChildView.as_view(), name="representative__child"),
