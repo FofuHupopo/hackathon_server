ab7f6410fe199deb74e03b8620a20fd7cf422109
FofuHupopo <fofuhupopo@gmail.com>
2022-12-04 11:52:28 +0500

    cors again x2

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -10,7 +10,9 @@ SECRET_KEY = 'django-insecure-%5(djd1)c5n8!x(&9rb(2m4xijc!4!g#w1n4wdh*j%b_e3trtj
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "*"
+]
 
 
 INSTALLED_APPS = [
@@ -21,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    "corsheaders",
     'rest_framework',
     
     # API
@@ -36,6 +39,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,9 +126,6 @@ SIMPLE_JWT = {
     'AUTH_COOKIE_SAMESITE': "Lax",
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
